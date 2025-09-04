080fbd141479ff0c85d87de6eb74df8f394bc619
SalmanAzamDev <salmankhach@gmail.com>
2025-09-04 15:02:38 +0500

    Added auth navigator

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,37 +1,15 @@
-// src/navigation/AuthNavigator.tsx
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/auth/LoginScreen";
+import SignupScreen from "../screens/auth/SignupScreen";
 
-import SplashScreen from '../screens/onboarding/Splash';
-import WelcomeScreen from '../screens/onboarding/Welcome';
-import WalkthroughOne from '../screens/onboarding/WalkOne';
-import WalkthroughTwo from '../screens/onboarding/WalkTwo';
-import WalkthroughThree from '../screens/onboarding/WalkThree';
-// Later: import Login, Signup, etc.
+const Stack = createNativeStackNavigator();
 
-export type AuthStackParamList = {
-  Splash: undefined;
-  Welcome: undefined;
-  WalkthroughOne: undefined;
-  WalkthroughTwo: undefined;
-  WalkthroughThree: undefined;
-};
-
-const Stack = createNativeStackNavigator<AuthStackParamList>();
-
-const AuthNavigator = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{ headerShown: false }}
-      initialRouteName="Splash"
-    >
-      <Stack.Screen name="Splash" component={SplashScreen} />
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="WalkthroughOne" component={WalkthroughOne} />
-      <Stack.Screen name="WalkthroughTwo" component={WalkthroughTwo} />
-      <Stack.Screen name="WalkthroughThree" component={WalkthroughThree} />
-    </Stack.Navigator>
-  );
-};
+const AuthNavigator = () => (
+  <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Signup" component={SignupScreen} />
+  </Stack.Navigator>
+);
 
 export default AuthNavigator;
