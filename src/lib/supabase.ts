b9a621cb756aeb909ffc1e6ecb4f71d067536933
SalmanAzamDev <salmankhach@gmail.com>
2025-09-09 11:17:09 +0500

    Added env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from 'react-native-dotenv';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL!, SUPABASE_ANON_KEY!, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
