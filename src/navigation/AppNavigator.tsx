0b7976743d954a61d1ed0914548259db39b82348
SalmanAzamDev <salmankhach@gmail.com>
2025-09-09 10:48:10 +0500

    Updated AppNavigator to use Auth Context

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import OnboardingNavigator from "./OnboardingNavigator";
-import AuthNavigator from "./AuthNavigator";
-import TabNavigator from "./TabNavigator";
+// src/navigation/AppNavigator.tsx
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ActivityIndicator, View } from 'react-native';
+import OnboardingNavigator from './OnboardingNavigator';
+import AuthNavigator from './AuthNavigator';
+import TabNavigator from './TabNavigator';
+import { useAuth } from '../context/AuthContext';
+import Colors from '@/utils/constants/colors';
 
 const Stack = createNativeStackNavigator();
 
-const AppNavigator = ({ isFirstLaunch, isAuthenticated }: { isFirstLaunch: boolean, isAuthenticated: boolean }) => {
+const AppNavigator = ({ isFirstLaunch }: { isFirstLaunch: boolean }) => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {isFirstLaunch ? (
         <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
-      ) : !isAuthenticated ? (
+      ) : !user ? (
         <Stack.Screen name="Auth" component={AuthNavigator} />
       ) : (
         <Stack.Screen name="Main" component={TabNavigator} />
