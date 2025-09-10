6f328be923497d52216718b67492562a0b0a9fae
SalmanAzamDev <salmankhach@gmail.com>
2025-09-10 11:29:02 +0500

    Simplified GoogleSignin.configure to use webClientId only

--- src/lib/googleSignIn.ts
+++ src/lib/googleSignIn.ts
@@ -1,8 +1,8 @@
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 import { 
   GOOGLE_WEB_CLIENT_ID, 
-  GOOGLE_ANDROID_CLIENT_ID, 
+//   GOOGLE_ANDROID_CLIENT_ID, 
 //   GOOGLE_IOS_CLIENT_ID 
 } from '@env';
 
@@ -10,7 +10,7 @@ export const configureGoogleSignIn = () => {
   GoogleSignin.configure({
     webClientId: GOOGLE_WEB_CLIENT_ID,
     // iosClientId: Platform.OS === 'ios' ? GOOGLE_IOS_CLIENT_ID : undefined,
-    androidClientId: Platform.OS === 'android' ? GOOGLE_ANDROID_CLIENT_ID : undefined,
+    // androidClientId: Platform.OS === 'android' ? GOOGLE_ANDROID_CLIENT_ID : undefined,
     offlineAccess: true,
   });
 };
