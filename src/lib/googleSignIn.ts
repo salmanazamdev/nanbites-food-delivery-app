46d4c08efd2c96c89006fe704bf392a6823a1ebe
SalmanAzamDev <salmankhach@gmail.com>
2025-09-10 10:48:41 +0500

    Configured Google Sign-In

---
+++ src/lib/googleSignIn.ts
@@ -0,0 +1,16 @@
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { Platform } from 'react-native';
+import { 
+  GOOGLE_WEB_CLIENT_ID, 
+  GOOGLE_ANDROID_CLIENT_ID, 
+  GOOGLE_IOS_CLIENT_ID 
+} from '@env';
+
+export const configureGoogleSignIn = () => {
+  GoogleSignin.configure({
+    webClientId: GOOGLE_WEB_CLIENT_ID,
+    iosClientId: Platform.OS === 'ios' ? GOOGLE_IOS_CLIENT_ID : undefined,
+    androidClientId: Platform.OS === 'android' ? GOOGLE_ANDROID_CLIENT_ID : undefined,
+    offlineAccess: true,
+  });
+};
